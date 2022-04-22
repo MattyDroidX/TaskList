6455272c4da6204778cd87eace6216a9d51371f4
MattyDroidX <fernando.matias.duarte@gmail.com>
2022-04-22 14:03:04 -0300

    interface created

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from '../app/Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Create ppt of js',
+    day: '20 April at 12:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Mastery structure flow',
+    day: '21 April at 12:00',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Design print',
+    day: '21 April at 14:00',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Create DB ppt',
+    day: '22 April at 14:00',
+    reminder: true,
+  },
+];
